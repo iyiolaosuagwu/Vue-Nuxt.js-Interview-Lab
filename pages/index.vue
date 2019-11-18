a3ca394e2789cf6377671bb189ea8188f339373b
iyiolaosuagwu <iyiolaipucci@gmail.com>
2019-11-18 16:23:31 +0100

    added new style

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="">
     <nav
-      class="navbar navbar-expand-lg navbar-dark bg-success"
-      style="backgroung: #3a8d41;"
+      class="navbar navbar-expand-lg navbar-dark"
+      style="background: #3a8d41 !important;"
     >
       <div class="container">
         <a class="navbar-brand" href="#">
@@ -20,7 +20,7 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
+          <ul class="navbar-nav ml-auto text-uppercase">
             <li class="nav-item active">
               <a class="nav-link" href="#">Login</a>
             </li>
@@ -89,112 +89,24 @@ import { ContentLoader } from "vue-content-loader";
 
 export default {
   state: {
-    isLoading: true
+    isLoading: true,
+    items: SHOP_DATA
   },
   components: {
     ContentLoader
   },
   data: function() {
-    return {
-      items: [
-        {
-          id: 1,
-          name: "Brown Brim",
-          imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-          price: 25
-        },
-        {
-          id: 2,
-          name: "Blue Beanie",
-          imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-          price: 18
-        },
-        {
-          id: 3,
-          name: "Brown Cowboy",
-          imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
-          price: 35
-        },
-        {
-          id: 4,
-          name: "Grey Brim",
-          imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
-          price: 25
-        },
-        {
-          id: 5,
-          name: "Green Beanie",
-          imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-          price: 18
-        },
-        {
-          id: 6,
-          name: "Palm Tree Cap",
-          imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-          price: 14
-        },
-        {
-          id: 7,
-          name: "Red Beanie",
-          imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-          price: 18
-        },
-        {
-          id: 8,
-          name: "Wolf Cap",
-          imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-          price: 14
-        },
-        {
-          id: 9,
-          name: "Blue Snapback",
-          imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-          price: 16
-        },
-        {
-          id: 30,
-          name: "Camo Down Vest",
-          imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-          price: 325
-        },
-        {
-          id: 31,
-          name: "Floral T-shirt",
-          imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-          price: 20
-        },
-        {
-          id: 32,
-          name: "Black & White Longsleeve",
-          imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
-          price: 25
-        },
-        {
-          id: 33,
-          name: "Pink T-shirt",
-          imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-          price: 25
-        },
-        {
-          id: 34,
-          name: "Jean Long Sleeve",
-          imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
-          price: 40
-        },
-        {
-          id: 35,
-          name: "Burgundy T-shirt",
-          imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
-          price: 25
-        }
-      ]
-    };
+    return SHOP_DATA;
   },
   components: {
     Logo
   },
   mounted: function() {},
-  methods: {},
+  methods: {
+    methodMap: function() {
+      console.log(SHOP_DATA);
+    }
+  },
   computed: {}
 };
 </script>
@@ -230,34 +142,4 @@ body {
   width: 100%;
   object-fit: cover;
 }
-/* .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-} */
 </style>
